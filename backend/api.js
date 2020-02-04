30e184418bd9c63d48806e3d451ec50c9a14ab2d
Akshat <akshatbhargavadeepa@gmail.com>
2020-02-05 04:06:35 +0530

    video durations now being returned from API

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require("child_process");
+const { getVideoDurationInSeconds } = require('get-video-duration')
 const path = require('path');
 const fs = require('fs');
 
@@ -8,25 +9,43 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const filesMap = {
+	REACT: {
+		files: [
+			'Introduction.mp4',
+			'Components.mp4',
+			'State.mp4',
+			'Props.mp4',
+		]
+	}
+};
+
 function generate(fileNames) {
 	return new Promise(resolve => {
 		const videosList = fileNames.map(file => `file '../public/videos/${file}.mp4'`);
 		const videoStrings = videosList.join('\n');
 		fs.writeFileSync('./temp.txt', videoStrings);
-		exec(`ffmpeg -f concat -safe 0 -i ${path.join(__dirname, 'temp.txt')} -c copy course.mp4`, (error, result) => {
+
+		// calculate durations
+		const durationPromises = fileNames.map(file => getVideoDurationInSeconds(path.join(__dirname, '../public/videos', file)));
+		Promise.all(durationPromises).then(durations => {
+			// merge videos
+			exec(`ffmpeg -f concat -safe 0 -i ${path.join(__dirname, 'temp.txt')} -c copy course.mp4`, (error, result) => {
 				if (error) return resolve(false);
-				return resolve(true);
+				return resolve({ durations });
 			});
+		});
+
 	});
 }
 
 app.post('/generate-video-content', (req, res) => {
-	const { files } = req.body;
-	generate(files).then(done => {
-		res.json({ done });
+	const { subCourseId } = req.body;
+	generate(filesMap.REACT.files).then(durations => {
+		res.json({ durations });
 	});
 });
 
-generate();
-
 app.listen(5000, () => console.log('Started on PORT 5000.'));
+
+generate(filesMap.REACT.files);
